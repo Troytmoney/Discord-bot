f941d4c3f399506f970af4c5d52cedecd2d2a69c
Troytmoney <89490065+Troytmoney@users.noreply.github.com>
2021-11-17 15:58:25 -0600

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,23 +1,23 @@
 module.exports = {
-  Admins: ["UserID", "UserID"], //Admins of the bot
-  ExpressServer: true, //If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
+  Admins: ["455166097839357985", "907057705565442090"], //Admins of the bot
+  ExpressServer: false, //If you wanted to make the website run or not
+  DefaultPrefix: process.env.Prefix || "!", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
-  Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
+  Token: process.env.Token || "OTEwNjQ2NjQ5MzA5NzY1NjMy.YZV3yA.E5gGz69nQhz3bDXY5US7syqzwhI", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "910646649309765632", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "RSdiuqQGPWivBQHlV2ZyAnXze_ty99i9", //Discord Client Secret
+  Scopes: ["applications.commands"], //Discord OAuth2 Scopes
   ServerDeafen: true, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Pikachu is cute", //A Secret like a password
+  CookieSecret: "Troyryan12", //A Secret like a password
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
   Permissions: 2205281600, //Bot Inviting Permissions
-  Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
+  Website: process.env.Website || "", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
   
   Presence: {
     status: "online", // You can show online, idle, and dnd
@@ -36,7 +36,7 @@ module.exports = {
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "9b96f0ccde344084af80ef23d7ce1120", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "4e8e1890c2254d58998ea16c36ce621d", //Spotify Client Secret
   },
 };
